d45fccc7bffc3f1be9bcd915ec5c953e484653c4
Jake Pelrah <pelrahjsp@gmail.com>
2024-10-25 19:49:47 -0400

    refactor

--- src/providers/LMProvider.jsx
+++ src/providers/LMProvider.jsx
@@ -10,30 +10,39 @@ export default function LMProvider({ children }) {
     const mapRef = useRef(null)
     const markerRef = useRef(null)
 
-    const [location, setLocation] = useState({
-        "_id": "671c0c22e1d881679cf2ad2b",
-        "title": "Red Rocks Amphitheatre",
-        "description": "Red Rocks Amphitheatre, located just outside Denver, Colorado, is a breathtaking outdoor concert venue renowned for its stunning natural acoustics and dramatic red sandstone formations. Nestled in a picturesque setting, the amphitheater offers a unique blend of natural beauty and artistic performance, attracting world-class musicians and enthusiastic audiences alike. With a seating capacity of approximately 9,500, it provides an intimate experience while showcasing the beauty of the surrounding foothills. Known for its vibrant history, Red Rocks has hosted legendary acts from The Beatles to U2, making it a cherished landmark in the music world and a must-visit destination for music lovers and outdoor enthusiasts.",
-        "category": "venue",
-        "coords": {
-            "view": {
-                "latitude": 39.66515803,
-                "longitude": -105.20664675
+    const [location, setLocation] = useState(
+        {
+            "title": "Red Rocks Amphitheatre",
+            "description": "Red Rocks Amphitheatre, located just outside Denver, Colorado, is a breathtaking outdoor concert venue renowned for its stunning natural acoustics and dramatic red sandstone formations. Nestled in a picturesque setting, the amphitheater offers a unique blend of natural beauty and artistic performance, attracting world-class musicians and enthusiastic audiences alike. With a seating capacity of approximately 9,500, it provides an intimate experience while showcasing the beauty of the surrounding foothills. Known for its vibrant history, Red Rocks has hosted legendary acts from The Beatles to U2, making it a cherished landmark in the music world and a must-visit destination for music lovers and outdoor enthusiasts.",
+            "category": "venue",
+            "camera": {
+                "tilt": 60,
+                "range": 100,
+                "heading": 77.86
+
             },
-            "marker": {
-                "latitude": 39.665542801567476,
-                "longitude": -105.20513606834602
-            }
-        },
-        "created": "2024-10-24T12:34:56-04:00",
-        "websites": {
-            "wikipedia": "https://en.wikipedia.org/wiki/Red_Rocks_Amphitheatre",
-            "default": "https://www.redrocksonline.com/",
-            "youtube": "https://www.youtube.com/channel/UCK36AQt-u4CLisfkFtkIxYw"
-        },
-        "image_url": "red_rocks.png",
-        "address": "18300 W Alameda Pkwy, Morrison, CO 80465"
-    })
+            "coords": {
+                "view": {
+                    "latitude": 39.66515803,
+                    "longitude": -105.20664675,
+                    "altitude": 2000
+                },
+                "marker": {
+                    "latitude": 39.665542801567476,
+                    "longitude": -105.20513606834602,
+                    "altitude":30
+                }
+            },
+            "created": "2024-10-24T12:34:56-04:00",
+            "websites": {
+                "wikipedia": "https://en.wikipedia.org/wiki/Red_Rocks_Amphitheatre",
+                "default": "https://www.redrocksonline.com/",
+                "youtube": "https://www.youtube.com/channel/UCK36AQt-u4CLisfkFtkIxYw"
+            },
+            "image_url": "redrocks.png",
+            "address": "18300 W Alameda Pkwy, Morrison, CO 80465"
+        }
+    )
 
 
     useEffect(() => {
@@ -47,14 +56,25 @@ export default function LMProvider({ children }) {
 
     useEffect(() => {
         if (location._id && markerRef.current) {
-            markerRef.current.position = { lat: location.coords.marker.latitude, lng: location.coords.marker.longitude, altitude: 100 }
+            markerRef.current.position = {
+                lat: location.coords.marker.latitude,
+                lng: location.coords.marker.longitude,
+                altitude: 30
+            }
             markerRef.current.label = location.title
+            markerRef.current.altitudeMode = 'RELATIVE_TO_GROUND'
+            markerRef.current.extruded = true
 
             mapRef?.current?.flyCameraTo({
                 endCamera: {
-                    center: { lat: location.coords.view.latitude, lng: location.coords.view.longitude, altitude: 2000 },
-                    tilt: 67.5,
-                    range: 1000
+                    center: {
+                        lat: location.coords.view.latitude,
+                        lng: location.coords.view.longitude,
+                        altitude: location.coords.marker.altitude
+                    },
+                    tilt: location.camera.tilt,
+                    range: location.camera.range,
+                    heading: location.camera.heading
                 },
                 durationMillis: 5000
             });
@@ -68,15 +88,25 @@ export default function LMProvider({ children }) {
         const { Map3DElement, Marker3DElement } = await google.maps.importLibrary("maps3d");
 
         const map = new Map3DElement({
-            center: { lat: location.coords.view.latitude, lng: location.coords.view.longitude, altitude: 2000 },
-            tilt: 60,
-            range: 100,
-            defaultLabelsDisabled: true,
-            heading: 77.86
+            center: {
+                lat: location.coords.view.latitude,
+                lng: location.coords.view.longitude,
+                altitude: location.coords.view.altitude
+            },
+            tilt: location.camera.tilt,
+            range: location.camera.range,
+            heading: location.camera.heading,
+            // defaultLabelsDisabled: true,
         });
 
         const marker = new Marker3DElement({
-            position: { lat: location.coords.marker.latitude, lng: location.coords.marker.longitude },
+            position: {
+                lat: location.coords.marker.latitude,
+                lng: location.coords.marker.longitude,
+                altitude: location.coords.marker.altitude,
+            },
+            altitudeMode: 'RELATIVE_TO_GROUND',
+            extruded: true,
             label: location.title
         });
 
