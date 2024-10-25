b20c4df28d3d199c1d10d564f581b291a2817dd2
Jake Pelrah <pelrahjsp@gmail.com>
2024-10-25 15:27:54 -0400

    mock data

--- src/components/MapPage.jsx
+++ src/components/MapPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from 'react'
-import { BsWikipedia, BsYoutube } from "react-icons/bs";
+import { BsWikipedia, BsYoutube, BsLink45Deg } from "react-icons/bs";
 import { BsPersonCircle } from "react-icons/bs";
 import { useLM } from '../providers/LMProvider';
 import MapNav from './MapNav'
@@ -72,27 +72,17 @@ function Info() {
           <span className="address">- 44123 Carrolside Ave, Lancaster, CA</span>
         </div>
         <p>
-          Lorem ipsum odor amet, consectetuer adipiscing elit. Etiam montes ante mus cubilia cras lectus. Tellus vel eget ligula sit; justo cras. Condimentum pellentesque duis amet pharetra molestie odio magnis pharetra. Iaculis mollis dis etiam ullamcorper maximus phasellus faucibus nam orci. Vulputate odio fames litora aenean congue hendrerit nostra sem venenatis.
-
-          Velit felis porttitor id odio odio rutrum. Fames curabitur vestibulum mattis egestas arcu orci potenti curae fames. Aptent consequat feugiat class; nostra pretium posuere parturient. Dictum sodales cursus class massa; maximus finibus aliquet quis. Mus molestie donec in facilisis fringilla. Tempus maecenas nibh integer commodo luctus quis odio non. Litora habitasse pulvinar dapibus finibus sodales consectetur. Rhoncus litora feugiat conubia sagittis ligula in vitae suspendisse viverra. Luctus bibendum nulla magnis hendrerit nunc curae.
-          Lorem ipsum odor amet, consectetuer adipiscing elit. Etiam montes ante mus cubilia cras lectus. Tellus vel eget ligula sit; justo cras. Condimentum pellentesque duis amet pharetra molestie odio magnis pharetra. Iaculis mollis dis etiam ullamcorper maximus phasellus faucibus nam orci. Vulputate odio fames litora aenean congue hendrerit nostra sem venenatis.
-
-          Velit felis porttitor id odio odio rutrum. Fames curabitur vestibulum mattis egestas arcu orci potenti curae fames. Aptent consequat feugiat class; nostra pretium posuere parturient. Dictum sodales cursus class massa; maximus finibus aliquet quis. Mus molestie donec in facilisis fringilla. Tempus maecenas nibh integer commodo luctus quis odio non. Litora habitasse pulvinar dapibus finibus sodales consectetur. Rhoncus litora feugiat conubia sagittis ligula in vitae suspendisse viverra. Luctus bibendum nulla magnis hendrerit nunc curae.
-          Lorem ipsum odor amet, consectetuer adipiscing elit. Etiam montes ante mus cubilia cras lectus. Tellus vel eget ligula sit; justo cras. Condimentum pellentesque duis amet pharetra molestie odio magnis pharetra. Iaculis mollis dis etiam ullamcorper maximus phasellus faucibus nam orci. Vulputate odio fames litora aenean congue hendrerit nostra sem venenatis.
-
-          Velit felis porttitor id odio odio rutrum. Fames curabitur vestibulum mattis egestas arcu orci potenti curae fames. Aptent consequat feugiat class; nostra pretium posuere parturient. Dictum sodales cursus class massa; maximus finibus aliquet quis. Mus molestie donec in facilisis fringilla. Tempus maecenas nibh integer commodo luctus quis odio non. Litora habitasse pulvinar dapibus finibus sodales consectetur. Rhoncus litora feugiat conubia sagittis ligula in vitae suspendisse viverra. Luctus bibendum nulla magnis hendrerit nunc curae.
-          Lorem ipsum odor amet, consectetuer adipiscing elit. Etiam montes ante mus cubilia cras lectus. Tellus vel eget ligula sit; justo cras. Condimentum pellentesque duis amet pharetra molestie odio magnis pharetra. Iaculis mollis dis etiam ullamcorper maximus phasellus faucibus nam orci. Vulputate odio fames litora aenean congue hendrerit nostra sem venenatis.
-
-          Velit felis porttitor id odio odio rutrum. Fames curabitur vestibulum mattis egestas arcu orci potenti curae fames. Aptent consequat feugiat class; nostra pretium posuere parturient. Dictum sodales cursus class massa; maximus finibus aliquet quis. Mus molestie donec in facilisis fringilla. Tempus maecenas nibh integer commodo luctus quis odio non. Litora habitasse pulvinar dapibus finibus sodales consectetur. Rhoncus litora feugiat conubia sagittis ligula in vitae suspendisse viverra. Luctus bibendum nulla magnis hendrerit nunc curae.
+          Red Rocks Amphitheatre, located just outside Denver, Colorado, is a breathtaking outdoor concert venue renowned for its stunning natural acoustics and dramatic red sandstone formations. Nestled in a picturesque setting, the amphitheater offers a unique blend of natural beauty and artistic performance, attracting world-class musicians and enthusiastic audiences alike. With a seating capacity of approximately 9,500, it provides an intimate experience while showcasing the beauty of the surrounding foothills. Known for its vibrant history, Red Rocks has hosted legendary acts from The Beatles to U2, making it a cherished landmark in the music world and a must-visit destination for music lovers and outdoor enthusiasts.
         </p>
       </div>
 
       <div id="bio" className="d-flex flex-column">
-        <img className="info-image" src="images/Zappa_16011977_01_300.jpg"></img>
-        <span className="text text-center">Frank Zappa</span>
+        <img className="info-image" src="images/RedRocksAMP.png"></img>
+        <span className="text text-center">Red Rocks</span>
         <div className="d-flex justify-content-center gap-3 mt-2">
-          <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Frank_Zappa"><BsWikipedia size={26} /></a>
-          <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/channel/UCj-Qb2d6dJJJtNL4rgr3FNw"><BsYoutube size={26} /></a>
+          <a target="_blank" rel="noopener noreferrer" href="https://www.theredrocksamphitheater.com/"><BsLink45Deg size={26} /></a>
+          <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Red_Rocks_Amphitheatre"><BsWikipedia size={26} /></a>
+          <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/channel/UCK36AQt-u4CLisfkFtkIxYw"><BsYoutube size={26} /></a>
         </div>
       </div>
 
