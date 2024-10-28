136398b8d3097b6ff3d85b4478f3af21eb240d13
Jake Pelrah <pelrahjsp@gmail.com>
2024-10-28 11:59:40 -0400

    refactor

--- src/components/MyLandmarks.jsx
+++ src/components/MyLandmarks.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import { useMap } from "../providers/MapProvider";
 import { FaRegEdit, FaTrash } from "react-icons/fa";
-import { Modal } from "bootstrap";
 import './myLandmarks.css'
 
 
@@ -13,8 +12,8 @@ export default function MyLandmarks() {
 
 
     useEffect(() => {
-        editModalRef.current = new Modal(editModalRef.current)
-        removeModalRef.current = new Modal(removeModalRef.current)
+        editModalRef.current = new bootstrap.Modal(editModalRef.current)
+        removeModalRef.current = new bootstrap.Modal(removeModalRef.current)
     }, [])
 
     function edit(e, lm) {
@@ -61,7 +60,6 @@ export default function MyLandmarks() {
             </div>
             <DeleteModal modalRef={removeModalRef} currentLandmark={currentLandmark} removeLandmark={removeLandmark} />
             <EditModal modalRef={editModalRef} currentLandmark={currentLandmark} updateLandmark={updateLandmark} />
-
         </>
     )
 }
@@ -74,9 +72,7 @@ function EditModal({ modalRef, currentLandmark, updateLandmark }) {
     const [wikiURL, setWikiURL] = useState('')
     const [youTubeURL, setYouTubeURL] = useState('')
 
-
-
-    useEffect(()=>{
+    useEffect(() => {
         setlandMarkTitle(currentLandmark?.title)
         setlandMarkDescription(currentLandmark?.description)
         setDefaultURL(currentLandmark?.websites.default)
@@ -84,12 +80,25 @@ function EditModal({ modalRef, currentLandmark, updateLandmark }) {
         setYouTubeURL(currentLandmark?.websites.youtube)
     },[currentLandmark])
 
-  
+
+    function update(e) {
+        e.preventDefault()
+        if (landMarkTitle && landMarkDescription && defaultURL && wikiURL && youTubeURL) {
+            console.log(landMarkTitle, landMarkDescription, defaultURL, wikiURL, youTubeURL)
+            modalRef.current.hide()
+        }
+        else {
+            alert('Fill out all fields')
+        }
+    }
+
+
+
     return (<div class="modal" ref={modalRef} tabindex="-1">
-        <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-dialog modal-lg modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title">Modal title</h5>
+                    <h5 class="modal-title">Editing</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
@@ -97,7 +106,7 @@ function EditModal({ modalRef, currentLandmark, updateLandmark }) {
                     <div id="create-form" >
 
                         <div class="row">
-            
+
                             <div class="col">
                                 <label for="floatingInput">Landmark Title</label>
                                 <input value={landMarkTitle} onChange={(e) => setlandMarkTitle(e.target.value)} type="text" className="form-control" />
@@ -110,22 +119,22 @@ function EditModal({ modalRef, currentLandmark, updateLandmark }) {
                                 <label for="floatingTextarea">Landmark Description</label>
                                 <textarea
                                     value={landMarkDescription} onChange={(e) => setlandMarkDescription(e.target.value)}
-                                    style={{ 'height': '6.2em' }}
+                                    style={{ 'height': '10em' }}
                                     class="form-control"></textarea>
                             </div>
 
                             <div class="col">
                                 <div class="">
                                     <label for="floatingPassword">Default URL</label>
-                                    <input value={defaultURL} onChange={(e)=>setDefaultURL(e.target.value)} type="text" className="form-control" />
+                                    <input value={defaultURL} onChange={(e) => setDefaultURL(e.target.value)} type="text" className="form-control" />
                                 </div>
                                 <div class="">
                                     <label for="floatingPassword">Wiki URL</label>
-                                    <input value={wikiURL} onChange={(e)=>setWikiURL(e.target.value)} type="text" className="form-control" />
+                                    <input value={wikiURL} onChange={(e) => setWikiURL(e.target.value)} type="text" className="form-control" />
                                 </div>
                                 <div class="">
                                     <label for="floatingPassword">YouTube URL</label>
-                                    <input value={youTubeURL} onChange={(e)=>setYouTubeURL(e.target.value)} type="text" className="form-control" />
+                                    <input value={youTubeURL} onChange={(e) => setYouTubeURL(e.target.value)} type="text" className="form-control" />
                                 </div>
                             </div>
 
@@ -133,18 +142,25 @@ function EditModal({ modalRef, currentLandmark, updateLandmark }) {
 
                     </div>
 
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button onClick={(e) => update(e)} type="button" class="btn btn-primary">Save changes</button>
+                </div>
             </div>
         </div>
-    </div>
     </div >)
 }
 
 
 function DeleteModal({ modalRef, currentLandmark, removeLandmark }) {
+
+    function remove(e, id){
+        e.preventDefault()
+        removeLandmark(id)
+        modalRef.current.hide()
+    }
+
     return (<div class="modal" ref={modalRef} tabindex="-1">
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
@@ -158,7 +174,7 @@ function DeleteModal({ modalRef, currentLandmark, removeLandmark }) {
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <div onClick={() => removeLandmark(currentLandmark._id)} type="button" class="btn btn-primary">Delete</div>
+                    <div onClick={(e) => remove(e, currentLandmark._id)} type="button" class="btn btn-primary">Delete</div>
                 </div>
             </div>
         </div>
