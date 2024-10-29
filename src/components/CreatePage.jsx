fe30bc5ce30201113fe514a97f80fc203cbe357c
Jake Pelrah <pelrahjsp@gmail.com>
2024-10-29 10:48:21 -0400

    css

--- src/components/CreatePage.jsx
+++ src/components/CreatePage.jsx
@@ -17,8 +17,9 @@ export default function CreatePage() {
     const [longitude, setLongitude] = useState(0)
     const [landMarkTitle, setlandMarkTitle] = useState('')
     const [landMarkDescription, setlandMarkDescription] = useState('')
-    const [markerLabel, setMarkerLabel] = useState('')
     const [clickPosition, setClickPosition] = useState(null)
+    const [landMarkAddress, setlandMarkAddress] = useState(null)
+    const [markerAltitude, setMarkerAltitude] = useState(0)
 
     useEffect(() => {
         initMap()
@@ -29,23 +30,23 @@ export default function CreatePage() {
     useEffect(() => {
 
         if (clickPosition) {
-            console.log(clickPosition)
             if (markerRef.current) {
                 markerRef.current.remove()
             }
             const { lat, lng, altitude } = clickPosition
             markerRef.current.label = landMarkTitle || 'Landmark Title'
-            markerRef.current.position = { lat, lng, altitude }
+          
+            markerRef.current.position = { lat, lng, altitude:parseFloat(markerAltitude) }
             mapRef.current.append(markerRef.current)
         }
-    }, [clickPosition])
+    }, [clickPosition, markerAltitude])
 
 
-    useEffect(()=>{
-        if(markerRef.current){
-            markerRef.current.label = landMarkTitle ||'Landmark Title'
+    useEffect(() => {
+        if (markerRef.current) {
+            markerRef.current.label = landMarkTitle || 'Landmark Title'
         }
-    },[landMarkTitle])
+    }, [landMarkTitle])
 
     function initMap() {
         loader.importLibrary('maps3d')
@@ -133,15 +134,31 @@ export default function CreatePage() {
     }
 
     // zoom to location
-    function zoomToViewport(geometry) {
+   async function zoomToViewport(geometry) {
         if (mapRef.current) {
-            mapRef.current.center = { lat: geometry.location.lat(), lng: geometry.location.lng(), altitude: 500 };
+            let elevation = await getElevationforPoint(geometry.location);
+            setMarkerAltitude(elevation)
+            mapRef.current.center = { lat: geometry.location.lat(), lng: geometry.location.lng(), altitude: elevation + 50 };
             mapRef.current.heading = 0;
             mapRef.current.range = 1000;
             mapRef.current.tilt = 65;
         }
     };
 
+    async function getElevationforPoint(location) {
+        const { ElevationService } = await google.maps.importLibrary("elevation");
+        // Get place elevation using the ElevationService.
+        const elevatorService = new google.maps.ElevationService();
+        const elevationResponse = await elevatorService.getElevationForLocations({
+          locations: [location],
+        });
+        if (!(elevationResponse.results && elevationResponse.results.length)) {
+          window.alert(`Insufficient elevation data for place: ${place.name}`);
+          return;
+        }
+        const elevation = elevationResponse.results[0].elevation || 10;
+        return elevation;
+      }
 
     function submit() {
 
@@ -149,95 +166,104 @@ export default function CreatePage() {
 
     return (
         <div className="">
+
             <div ref={mapElemRef} id="create-map"></div>
 
             <table id="create-table" className="table">
                 <thead>
                     <tr>
+                        <th scope="col">Search</th>
                         <th scope="col">Alitude</th>
                         <th scope="col">Latitude</th>
                         <th scope="col">Longitude</th>
                         <th scope="col">Tilt</th>
                         <th scope="col">Heading</th>
                         <th scope="col">Range</th>
-                        <th scope="col">Roll</th>
+                        <th scope="col">Marker Altitude</th>
+
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
+                        <td> <input className="form-control" ref={searchRef} type="text"></input></td>
                         <td>{altitude?.toFixed(2)}</td>
                         <td>{latitude?.toFixed(2)}</td>
                         <td>{longitude?.toFixed(2)}</td>
                         <td>{tilt?.toFixed(2)}</td>
                         <td>{heading?.toFixed(2)}</td>
                         <td>{range?.toFixed(2)}</td>
-                        <td>{roll?.toFixed(2)}</td>
+                        <td><input value={markerAltitude} onChange={(e)=>setMarkerAltitude(e.target.value)} type="number"></input></td>
                     </tr>
 
                 </tbody>
             </table>
 
-            <div id="create-form" className="mx-5 px-5">
 
-                <div class="mb-3">
-                    <label for="floatingInput">Search</label>
-                    <input className="form-control" ref={searchRef} type="text"></input>
-                </div>
+            <div className="d-flex justify-content-center gap-5 mx-5">
 
-                <div class="row">
+                <div className="d-flex flex-column flex-fill gap-2">
 
-                    <div class="col">
-                        <div class="m">
-                            <label for="floatingInput">Landmark Title</label>
-                            <input value={landMarkTitle} onChange={(e) => setlandMarkTitle(e.target.value)} type="text" className="form-control" />
-                        </div>
+                    <div>
+                        <label for="floatingInput">Landmark Title</label>
+                        <input value={landMarkTitle} onChange={(e) => setlandMarkTitle(e.target.value)} type="text" className="form-control" />
                     </div>
 
-
-                    <div class="col">
-                        <div class="">
-                            <label className="custom-file-upload">Choose landmark image:</label>
-                            <input type="file" id="landmark-image" accept="image/png, image/jpeg" />
-                        </div>
+                    <div>
+                        <label for="floatingInput">Landmark Address</label>
+                        <input value={landMarkAddress} onChange={(e) => setlandMarkAddress(e.target.value)} type="text" className="form-control" />
                     </div>
-                </div>
 
-                <div class="row">
-                    <div class="col">
+                    <div>
                         <label for="floatingTextarea">Landmark Description</label>
                         <textarea
                             value={landMarkDescription} onChange={(e) => setlandMarkDescription(e.target.value)}
-                            style={{ 'height': '5em' }}
+                            style={{ 'height': '6em' }}
                             class="form-control"></textarea>
                     </div>
+
                 </div>
 
+                <div className="d-flex flex-column flex-fill gap-1">
 
-                <div class="row">
-                    <div class="col">
-                        <div class="">
-                            <label for="floatingPassword">Default URL</label>
-                            <input type="text" className="form-control" />
-                        </div>
+
+                <select class="form-select mt-4" aria-label="Default select example">
+                        <option selected>Select category</option>
+                        <option value="artist">Artist</option>
+                        <option value="genre">Genre</option>
+                        <option value="studio">Studio</option>
+                        <option value="venue">Venue</option>
+
+                    </select>
+                    <div>
+                        <label for="floatingPassword">Default URL</label>
+                        <input type="text" className="form-control" />
                     </div>
-                    <div class="col">
-                        <div class="">
-                            <label for="floatingPassword">Wiki URL</label>
-                            <input type="text" className="form-control" />
-                        </div>
+
+                    <div>
+                        <label for="floatingPassword">Wiki URL</label>
+                        <input type="text" className="form-control" />
                     </div>
-                    <div class="col">
-                        <div class="">
-                            <label for="floatingPassword">YouTube URL</label>
-                            <input type="text" className="form-control" />
-                        </div>
+
+                    <div>
+                        <label for="floatingPassword">YouTube URL</label>
+                        <input type="text" className="form-control" />
                     </div>
+
                 </div>
 
+                <div className="d-flex flex-column">
 
+                    <div className="mt-4 mb-5">
+                        <label for="landmark-image" className="custom-file-upload">Choose landmark image:</label>
+                        <input type="file" id="landmark-image" accept="image/png, image/jpeg" />
+                    </div>
 
-                <button onSubmit={submit}>Create Landmark</button>
+                    <button id="create-btn" className="btn mt-5" onSubmit={submit}>Create Landmark</button>
+
+                </div>
+
             </div>
+
         </div>
     )
 }
